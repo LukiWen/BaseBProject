bd8c260904e7b999593aed0ccc9c5c8bc6342f54
LukiWen <lukiwen19@126.com>
2018-03-07 16:56:38 +0800

    新增类

---
+++ BaseBProject/BaseView.swift
@@ -0,0 +1,21 @@
+//
+//  BaseView.swift
+//  BaseBProject
+//
+//  Created by chuxin on 2018/3/7.
+//  Copyright © 2018年 wenhelu. All rights reserved.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
