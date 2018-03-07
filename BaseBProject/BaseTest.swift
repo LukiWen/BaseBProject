c682cc3028771f696ad81e28705898bf63ef223f
LukiWen <lukiwen19@126.com>
2018-03-07 09:55:58 +0800

    init

---
+++ BaseBProject/BaseTest.swift
@@ -0,0 +1,13 @@
+//
+//  BaseTest.swift
+//  BaseBProject
+//
+//  Created by chuxin on 2018/3/5.
+//  Copyright © 2018年 wenhelu. All rights reserved.
+//
+
+import UIKit
+
+class BaseTest: NSObject {
+
+}
---
+++ BaseBProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BaseBProject
+//
+//  Created by chuxin on 2018/3/5.
+//  Copyright © 2018年 wenhelu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
