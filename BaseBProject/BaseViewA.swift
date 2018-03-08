8fda6477c49d7fcd979626954e349fb045abae3f
LukiWen <lukiwen19@126.com>
2018-03-08 16:04:24 +0800

    新增测试类

---
+++ BaseBProject/BaseViewA.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewA.swift
+//  BaseBProject
+//
+//  Created by chuxin on 2018/3/8.
+//  Copyright © 2018年 wenhelu. All rights reserved.
+//
+
+import UIKit
+
+public class BaseViewA: UIView {
+
+    public func baseTst(){
+        print(123)
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
